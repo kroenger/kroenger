a0fd5cfe63084492b5e352a74372e63d2400eae3
Augu <augustine.ashal@sowlab.com>
2022-10-10 18:59:13 +0530

    update config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://kroenger.github.io',
+  base: '/kroenger',
   integrations: [react()]
 });
