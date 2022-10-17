cbddcba0749676ea7caf7f00da4d990a329a32f6
Augu <augustine.ashal@sowlab.com>
2022-10-17 22:44:48 +0530

    tips updated

--- src/assets/ecofriendlytips.js
+++ src/assets/ecofriendlytips.js
@@ -1,17 +1,244 @@
 export const ecofriendlytips = [
     {
-        tipNumber:1,
-        tip: "Instead of throwing away banana peels, blend it with an egg and apply to your scalp. This boost your hair growth and reduce grey hair.",
-        hashtags:["ecotips","healthhack","beautytip","haircare"]
+        tipNumber: 1,
+        tip: `"Use less" is the basis of eco friendly concept.
+        Reduce the consumption and maximize the utility.`,
+        hashtags: ["consumption", "utility", "useless", "reduce"]
     },
     {
-        tipNumber:2,
+        tipNumber: 2,
+        tip: `Always opt for a green alternative rather than a single use stuff.`,
+        hashtags: ["green", "alternative", "singleuse"]
+    },
+    {
+        tipNumber: 3,
+        tip: `Renounce plastic bottles, opt for a carbon-neutral one.`,
+        hashtags: ["banplasticbottle", "carbonneutral", "homecare"]
+    },
+    {
+        tipNumber: 4,
+        tip: `Use reusable shopping bags, than a plastic bag`,
+        hashtags: ["reusable", "shoppingbags", "plasticbag"]
+    },
+    {
+        tipNumber: 5,
+        tip: `Don't litter plastic, reuse it through molding.`,
+        hashtags: ["donotlitter", "reuse", "molding"]
+    },
+    {
+        tipNumber: 6,
+        tip: `How to become a sustainable traveller.
+        Dine from local places.
+        Purchase local stuff.
+        Carry eco-bags for trash.`,
+        hashtags: ["vocalforlocal", "ecobags", "sustainabletraveller"]
+    },
+    {
+        tipNumber: 7,
+        tip: `How to become a sustainable traveller.
+        Carry your accessories.
+        Stay at eco-friendly places.
+        Do no litter, not even biodegradable stuff.
+        Walk or cycle.`,
+        hashtags: ["walkforhealth", "biodegradable", "sustainabletraveller"]
+    },
+    {
+        tipNumber: 8,
+        tip: `Tips to save energy.
+        Change all of your light bulbs to LED.
+        Plug large electronics into a smart strip.`,
+        hashtags: ["saveenergy", "LED", "homecare"]
+    },
+    {
+        tipNumber: 9,
+        tip: `Tips to save energy.
+        Get an energy audit.
+        Check weatherproofing on all windows.`,
+        hashtags: ["saveenergy", "weatherproofing", "energyaudit"]
+    },
+    {
+        tipNumber: 10,
+        tip: `Towards sustainable food management.
+        Compost your food and yard waste.
+        Grow your own herbs.`,
+        hashtags: ["lemon", "foodwaste", "growyourherbs"]
+    },
+    {
+        tipNumber: 11,
+        tip: `Towards sustainable food management.
+        Shop at your local farmers market.
+        Zero waste of food shopping.`,
+        hashtags: ["foodmanagement", "localfarmers", "zerowaste"]
+    },
+    {
+        tipNumber: 12,
+        tip: `Towards sustainable food management.
+        Follow the protocols of proper recycling.
+        Deny all plastic bags.`,
+        hashtags: ["foodmanagement", "recycling", "plasticbags"]
+    },
+    {
+        tipNumber: 13,
+        tip: `Towards sustainable food management.
+        Shop at thrift stores.
+        Purchase sustainable toothbrushes and reusable ear swabs.`,
+        hashtags: ["foodmanagement", "thriftstores", "sustainabletoothbrushes"]
+    },
+    {
+        tipNumber: 14,
+        tip: `Beef is the higgest carbon footprint food product. 
+        Include less meat & more plant-based food in your diet for a better sustainable life.`,
+        hashtags: ["reducebeef", "plantbased", "sustainablelifestyle"]
+    },
+    {
+        tipNumber: 15,
+        tip: `Choose used plastic bags as grow bags rather than buying a new one.`,
+        hashtags: ["plasticbags", "growbags", "homecare"]
+    },
+    {
+        tipNumber: 16,
+        tip: `Use an activated charcoal stick rather than a purifier.`,
+        hashtags: ["activatedcharcoal", "purifier", "savewater"]
+    },
+    {
+        tipNumber: 17,
+        tip: `Avoid nylon based or microfibre bleached clothes.`,
+        hashtags: ["nylon", "microfibre", "sustainableclothes"]
+    },
+    {
+        tipNumber: 18,
+        tip: `Use plant or bamboo based toothbrush rather than a plastic toothbrush.`,
+        hashtags: ["toothbrush", "bambootoothbrush", "reduceplastic"]
+    },
+    {
+        tipNumber: 19,
+        tip: `Use cornstarch based compost bag instead of a compostable plastic bag.`,
+        hashtags: ["cornstarch", "compostbag", "plasticbag"]
+    },
+    {
+        tipNumber: 20,
+        tip: `Reuse clothes & reduce hoarding.`,
+        hashtags: ["reuse", "reduce", "stophoarding"]
+    },
+    {
+        tipNumber: 21,
+        tip: `Say no to plastic cutlery while ordering takeout.`,
+        hashtags: ["plasticcutlery", "eatout", "reduceplastic"]
+    },
+    {
+        tipNumber: 22,
+        tip: `Switch to plant based milk from diary.`,
+        hashtags: ["plantbasedmilk", "reducedairy", "globalwarming"]
+    },
+    {
+        tipNumber: 23,
+        tip: `Opt for a reusable cup, (eg. stainless steel cup) instead of a single-use plastic cup.`,
+        hashtags: ["reusablecup", "single-use", "plasticcup"]
+    },
+    {
+        tipNumber: 24,
+        tip: `Use plastic-free hair tools, such as wooden combs and brushes.`,
+        hashtags: ["plastic-free", "woodencombs", "skincare"]
+    },
+    {
+        tipNumber: 25,
+        tip: `Choose clothing made from organic materials or buy second-hand.`,
+        hashtags: ["organicmaterials", "secondhand", "usedclothes"]
+    },
+    {
+        tipNumber: 26,
+        tip: `Carry your own containers for take out and leftovers.`,
+        hashtags: ["containers", "leftovers", "donotwastefood"]
+    },
+    {
+        tipNumber: 27,
+        tip: `Create your own cleaning products using water and vinegar instead of buying cleaning products in plastic packaging.`,
+        hashtags: ["cleaning products", "instantdishwash", "homecare"]
+    },
+    {
+        tipNumber: 28,
+        tip: `Use natural cleaning scrubbers and sponges instead of using synthetic sponges.`,
+        hashtags: ["scrubbers", "sponges", "skincare"]
+    },
+    {
+        tipNumber: 29,
+        tip: `Store lemon inside refrigerator in a container filled with water to make it last for 3 months.`,
+        hashtags: ["refrigerator", "CFC", "ozone"]
+    },
+    {
+        tipNumber: 30,
         tip: "Make instant dish wash using lemon and salt for simple and eco-friendly washing.",
-        hashtags:["lemon","instantdishwash","homecare"]
+        hashtags: ["lemon", "instantdishwash", "homecare"]
     },
     {
-        tipNumber:3,
+        tipNumber: 31,
         tip: "Use non-plastic wrapers like paper, leaves, etc.",
-        hashtags:["plasticfree","reduceplastic","gosustainable"]
+        hashtags: ["plasticfree", "reduceplastic", "gosustainable"]
+    },
+    {
+        tipNumber: 32,
+        tip: `Replace hazardous PU Foam with Non-isocyanate Polyurethane (NIPU) which is recyclable & cheap to incinerate.`,
+        hashtags: ["hazardous", "recyclable", "incinerate"]
+    },
+    {
+        tipNumber: 33,
+        tip: `Demand bulk snacks packed in recyclable packaging to minimize non recyclable Multilayer Plastic (MLP) that blocks dumpsites & landfills.`,
+        hashtags: ["recyclable", "dumpsites", "landfills"]
+    },
+    {
+        tipNumber: 34,
+        tip: `Auction e-waste separately from other scrap for safe recycling by authorised agents.`,
+        hashtags: ["ewaste", "electronicwaste", "scrap"]
+    },
+    {
+        tipNumber: 35,
+        tip: `When designing new building demand natural lighting, cooling, or heating to build in power saving features.`,
+        hashtags: ["ecofriendlybuilding", "sustainablelifestyle", "homecaretips"]
+    },
+    {
+        tipNumber: 36,
+        tip: `Opt for energy efficient ACs when buying or replacing a new one.`,
+        hashtags: ["energyefficient", "airconditioner", "lifestyle"]
+    },
+    {
+        tipNumber: 37,
+        tip: `Deep freeze lemon juice and store them as cubes in ziplock bags to make it last for months.`,
+        hashtags: ["lemon", "storelemons", "storehacks"]
+    },
+    {
+        tipNumber: 38,
+        tip: `Use menstrual cups or reusable/biodegradable menstrual pads.`,
+        hashtags: ["reusable", "biodegradable", "menstrualcups"]
+    },
+    {
+        tipNumber: 39,
+        tip: `Store tomatoes upside down to make it last longer.`,
+        hashtags: ["vegetabletips", "storagetips", "homecaretips"]
+    },
+    {
+        tipNumber: 40,
+        tip: `Remove the plastic buttons before disposing your clothes & reuse it as spare buttons.`,
+        hashtags: ["plasticbuttons", "reduceplastic", "ecolifestyle"]
+    },
+    {
+        tipNumber: 41,
+        tip: `Use cold-pressed coconut oil as healthy moisturisers for ace and hair, instead of using creams with harsh chemicals sold in plastic containers.`,
+        hashtags: ["coconutoil", "plasticcontainers", "healthymoisturisers"]
+    },
+    {
+        tipNumber: 42,
+        tip: `Read your favourite newspapers and magazines online to reduce paper waste and save money.`,
+        hashtags: ["paperwaste", "newspapers", "readonline"]
+    },
+    {
+        tipNumber: 43,
+        tip: `Charge your phones in airplane mode for fast charging & to save energy.`,
+        hashtags: ["airplanemode", "saveenergy", "homecare"]
+    },
+    {
+        tipNumber: 44,
+        tip: `Instead of throwing away banana peels, blend it with an egg 
+        and apply to your scalp. This boost your hair growth and reduce grey hair."`,
+        hashtags: ["ecotips", "healthhack", "beautytip", "haircare"]
     }
 ]
