0cccd6a604c5487ff3cc23895031de2af6341233
Augu <augustine.ashal@sowlab.com>
2022-11-03 17:04:38 +0530

    data update for november

--- src/observanceDaysData/november.js
+++ src/observanceDaysData/november.js
@@ -6,6 +6,26 @@ export const november = [
         Vegan food reduces the risks of many diseases and provides you with healthier alternatives to satiate your cravings.`
     },
     {
+        date: "3 November",
+        day: "World Jellyfish Day",
+        description: `The jellyfish has a significant impact on the biomass, spatiotemporal dynamics, and community structure of 
+        the global marine plankton ecosystems.They provide food for us, house young fish in their tentacles, and are being studied
+         for potential medical applications.`
+    },
+    {
+        date: "3 November",
+        day: "One Health Day",
+        description: `One Health is a collaborative, multisectoral, and trans-disciplinary approach — working at the local,
+        regional, national, and global levels — with the goal of achieving optimal health outcomes recognizing the interconnection
+        between people, animals, plants, and their shared environment.`
+    },
+    {
+        date: "3 November",
+        day: "International Day for Biosphere Reserves",
+        description: `The World Network of Biosphere Reserves (WNBR) was formed in 1971, as a backbone for biodiversity conservation,
+        ecosystem restoration, and living in harmony with nature.`
+    },
+    {
         date: "6 November",
         day: "International day for preventing of exploitation of the environment in war and armed conflicts",
         description: `"We must use all of the tools at our disposal, from dialogue and mediation to preventive diplomacy, 
