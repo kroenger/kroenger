ddb3dc875444acdd81470d13c0655ba3bbb82277
Augu <augustine.ashal@sowlab.com>
2022-11-17 23:07:57 +0530

    november data update

--- src/observanceDaysData/november.js
+++ src/observanceDaysData/november.js
@@ -26,6 +26,14 @@ export const november = [
         ecosystem restoration, and living in harmony with nature.`
     },
     {
+        date: "5 November",
+        day: "World Tsunami Awareness Day",
+        description: `Tsunamis are rare events but can be extremely deadly. In the past 100 years, 58 of them have claimed more than 
+        260,000 lives, or an average of 4,600 per disaster, surpassing any other natural hazard. Learn the signs of a potential tsunami, 
+        such as an earthquake, a loud roar from the ocean, or unusual ocean behavior, such as a sudden rise or wall of water or sudden 
+        draining of water showing the ocean floor.`
+    },
+    {
         date: "6 November",
         day: "International day for preventing of exploitation of the environment in war and armed conflicts",
         description: `"We must use all of the tools at our disposal, from dialogue and mediation to preventive diplomacy, 
@@ -37,5 +45,35 @@ export const november = [
         day: "World Science Day for Peace and Development",
         description: `The Day highlights the significant role of science in society and the need to engage the wider public 
         in debates on emerging scientific issues. It also underlines the importance and relevance of science in daily living.`
+    },
+    {
+        date: "Third Thursday of November",
+        day: "Use Less Stuff Day",
+        description: `Use Less Stuff Day falls on the third Thursday of November every year and was created to spread awareness
+        about the clutter of unnecessary things in our homes. It encourages people to adopt a minimalist lifestyle where only 
+        the essentials are bought or kept.`
+    },
+    {
+        date: "19 November",
+        day: "World Toilet Day",
+        description: `This day is celebrated with the aim of tackling global sanitation crisis and achieve Sustainable 
+        Development Goal 6: water and sanitation for all by 2030. The World Toilet Day 2022 campaign, 
+        called 'Making the invisible visible', focuses on the impact of the sanitation crisis on groundwater, 
+        exploring how inadequate sanitation systems spread human waste into rivers, lakes and soil, polluting underground water resources.`
+    },
+    {
+        date: "26 November",
+        day: "Buy Nothing Day",
+        description: `Buy Nothing Day is a day of protest in which participants pledge to buy nothing for 24 hours 
+        to raise awareness of the negative environmental, social, and political consequences of overconsumption. 
+        It is typically observed in North America on the Friday following Thanksgiving Day in the United States, 
+        which is one of the busiest shopping days of the year.`
+    },
+    {
+        date: "27 November",
+        day: "Turtle Adoption Day",
+        description: `Different from World Turtle Day, World Turtle Adoption day focuses on the adoption and 
+        care of turtles that are stranded and are in need of love. Just as the theme of World Turtle Day 2022 
+        suggests, we can "SHELLEBRATE" this day and make 'everyone to Love and Save Turtles'.`
     },
 ]
