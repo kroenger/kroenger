b530d0455d810ddc05d6f425f3b60d1f660f7bc4
Augu <joseyaugustine18@gmail.com>
2022-12-26 20:39:08 +0530

    typo fix

--- src/observanceDaysData/january.js
+++ src/observanceDaysData/january.js
@@ -1,7 +1,7 @@
 export const january = [
     {
         date: "24 January",
-        day: "International Day of Eduation",
+        day: "International Day of Education",
         description: `Without inclusive and equitable quality education and lifelong opportunities for all, 
         countries will not succeed in achieving gender equality and breaking the cycle of poverty that is leaving 
         millions of children, youth and adults behind.`
