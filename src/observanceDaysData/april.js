fefc9666e5fc074e2a80a384f2628d32dd874d07
Augu <joseyaugustine18@gmail.com>
2023-03-31 13:41:14 +0530

    april data update

--- src/observanceDaysData/april.js
+++ src/observanceDaysData/april.js
@@ -2,17 +2,70 @@ export const april = [
     {
         date: "3 April",
         day: "World Aquatic Animal Day",
-        description: `The theme for 2022 is Disentangling the Sustainability Myth in Fishing and Aquaculture`
+        description: `The aim of this day is to raise awareness about the importance, challenges and threats 
+        that aquatic animals face, and to advocate for their protection through law, policy, education and action. 
+        The theme for 2023 is Alternatives to the Use of Aquatic Animals.`
+    },
+    {
+        date: "7 April",
+        day: "World Health Day",
+        description: `It is organized by the World Health Organization (WHO) and each year draws attention to a specific 
+        health topic of concern to people all over the world. The date of April 7th marks the anniversary of the founding 
+        of WHO in 1948. The theme for 2023 is Health For All, which coincides with WHO’s 75th birthday`
+    },
+    {
+        date: "7 April",
+        day: "International Beaver Day",
+        description: `A day to celebrate and appreciate beavers and their role in creating and maintaining wetlands. The aim of this 
+        day is to educate people about the amazing abilities of beavers, their benefits for the environment and wildlife, 
+        and the methods of coexisting with them peacefully.`
+    },
+    {
+        date: "16 April",
+        day: "Save the Elephant Day",
+        description: `Save the Elephant Day is a day to celebrate and protect elephants and their habitats. The purpose of this day is to raise 
+        awareness about the dangers elephants face, such as poaching, habitat loss, human-elephant conflict, and climate change. 
+        The day also encourages people to support organizations that work to save elephants from extinction, such as Save the Elephants.`
     },
     {
         date: "17 April",
         day: "International Bat Appreciation Day",
-        description: `Bats are critical to our ecosystem. Thanks to them, they get rid of many harmful or problematic insects, 
-        including mosquitoes. Apart from being the natural pest controllers, bats also pollinate flowers and their faeces are rich fertilisers.`
+        description: `A day to celebrate and protect bats and their habitats. Bats are critical to our ecosystem. Thanks to them, 
+        they get rid of many harmful or problematic insects, including mosquitoes. Apart from being the natural pest controllers, 
+        bats also pollinate flowers and their faeces are rich fertilisers.`
     },
     {
         date: "22 April",
         day: "Earth Day",
-        description: `The theme of Earth Day 2022 is to ‘Invest in our planet’.we need to act (boldly), innovate (broadly), and implement (equitably).`
+        description: `The theme for Earth Day 2023 is “Invest in Our Planet”, which calls for collective action and investment from governments, 
+        businesses and citizens to create a green, prosperous and equitable future for humanity and the planet. Earth Day was founded in 1970 
+        as a day of education and activism on environmental issues to protect and restore the Earth.`
     },
+    {
+        date: "25 April",
+        day: "World Penguin Day",
+        description: `A day to celebrate and learn about one of the most adorable and unique animal species in the world. 
+        It is observed on April 25 every year, which coincides with the annual migration of Adelie penguins in Antarctica. It aims to raise 
+        awareness about the threats that penguins face, such as climate change, overfishing, pollution, and habitat loss.`
+    },
+    {
+        date: "27 April",
+        day: "World Tapir Day",
+        description: `The day aims to educate people about these large herbivorous mammals that are often overlooked and misunderstood. 
+        Tapirs are important for the ecosystem as seed dispersers and indicators of forest health. However, they are threatened by 
+        habitat loss, hunting, and human encroachment and all tapir species are endangered or vulnerable.`
+    },
+    {
+        date: "Last Saturday of April",
+        day: "Save the Frogs Day",
+        description: `A day to celebrate and raise awareness about the diverse and fascinating amphibians that inhabit our planet. The day 
+        aims to educate people about the threats that frogs face, such as habitat loss, pollution, disease, invasive species, and climate change.`
+    },
+    {
+        date: "Last Saturday of April",
+        day: "World Veterinary Day",
+        description: `The day aims to raise awareness of the important role that veterinarians play in the health and welfare of animals, people, 
+        and the environment. Veterinarians not only treat sick and injured animals, but also prevent diseases, ensure food safety and quality, 
+        protect wildlife and biodiversity, and contribute to scientific research and innovation.  The theme for 2023 is "Promoting diversity, equity, and inclusiveness in the veterinary profession"`
+    }
 ]
