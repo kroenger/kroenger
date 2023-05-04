304f57b8cbe7a58d4f4f82caa7a4fd20c780c12a
Augu <joseyaugustine18@gmail.com>
2023-05-05 01:15:49 +0530

    may update

--- src/observanceDaysData/may.js
+++ src/observanceDaysData/may.js
@@ -1,21 +1,59 @@
 export const may = [
     {
-        date: "14 May",
+        date: "2 May",
+        day: "World Tuna Day",
+        description: `A day serves as a platform to raise awareness about the importance of tuna conservation, 
+        promote sustainable fishing practices, and encourage collaboration among stakeholders to ensure the 
+        future of tuna populations. When shopping look for the MSC blue fish tick label when shopping for tuna 
+        products, which indicates that they come from fisheries that meet the highest standards of sustainability 
+        and traceability.  There are seven species of tuna, and they are found in all the major oceans of the world, 
+        except for the polar regions.`
+    },
+    {
+        date: "12 May",
+        day: "International Day of Plant Health",
+        description: ` The theme for this year is Plant Health for Environmental Protection.
+        Using natural pesticides or other environmentally friendly methods prevents or controls plant pests and 
+        diseases, rather than relying on chemicals that can harm plants, soil, water, and biodiversity. Protecting 
+        plant health is a shared responsibility that requires cooperation and coordination among different actors, 
+        such as farmers, gardeners, researchers, policymakers, traders, consumers, and international organizations.`
+    },
+    {
+        date: "13 May",
         day: "World Migratory Bird Day",
-        description: `an annual awareness-raising campaign highlighting the need for the conservation of migratory birds and their habitats`
+        description: `This year’s theme is "Water: Sustaining Bird Life". The vast majority of migratory 
+        birds rely on aquatic ecosystems during their life cycles. Inland and coastal wetlands, rivers, 
+        lakes, streams, marshes, and ponds are all vital for feeding, drinking, or nesting, and also as 
+        places to rest and refuel during their long journeys. Unfortunately, aquatic ecosystems are becoming 
+        increasingly threatened around the world and so are the migratory birds that depend on them.`
     },
     {
         date: "20 May",
         day: "World Bee Day",
-        description: `Theme for the year 2022 is “Bee engaged: Build Back Better".
-        protect bees and other pollinators, which would significantly contribute to solving problems 
-        related to the global food supply and eliminate hunger in developing countries`
+        description: `Bees are essential pollinators for many crops, such as fruits, vegetables, nuts and seeds. Without bees, many crops would have lower yields and quality, 
+        or even fail to produce at all. We can all help by supporting organic and sustainable farming practices, planting bee-friendly plants in our gardens and balconies, 
+        avoiding or minimizing pesticide use, and providing water and shelter for bees.`
+    },
+    {
+        date: "22 May",
+        day: "International Day for Biological Diversity",
+        description: `The theme for 2023 is “From Agreement to Action: Build Back Biodiversity” which builds on the results of the 15th Conference of Parties 
+        to the UN Convention on Biological Diversity(COP-15) where the Kunming - Montreal Global Biodiversity Framework was adopted. This global framework plans 
+        to conserve and restore nature by 2030 and beyond. The theme also emphasizes the need to shift from agreement to action and implement the framework in order 
+        to build back biodiversity after the pandemic and other threats.`
     },
     {
         date: "23 May",
         day: "World Turtle Day",
-        description: `The day was created as a yearly observance to help people celebrate and protect 
-        turtles and tortoises and their disappearing habitats around the world as well as to encourage 
-        human action to help them survive and thrive`
+        description: `The theme for 2023 is “I Love Turtles!” which asks everyone to embrace their inner turtle and show their appreciation of these fantastic creatures. 
+        The day educates people about the threats that turtles and tortoises face, such as smuggling, habitat destruction, pet trade, and live exotic food markets. 
+        According to experts, turtles are the most threatened of the major groups of vertebrates, more so than birds, mammals, and fish.`
+    },
+    {
+        date: "31 May",
+        day: "World No Tobacco Day",
+        description: `The theme for 2023 is “We need food, not tobacco, " emphasising the importance of healthy food choices and prioritising food over tobacco. 
+        The 2023 campaign also aims to expose the tobacco industry’s efforts to interfere with attempts to substitute tobacco growing with sustainable crops, 
+        thereby contributing to the global food crisis. Tobacco growing and production exacerbates food insecurity, soil degradation, deforestation, and desertification.`
     },
 ]
