86d812e9c4571a33286f7dcecaf76a6d8cdbd625
Augu <joseyaugustine18@gmail.com>
2023-05-29 19:20:30 +0530

    june data update

--- src/observanceDaysData/june.js
+++ src/observanceDaysData/june.js
@@ -2,38 +2,83 @@ export const june = [
     {
         date: "1 June",
         day: "World Reef Day",
-        description: `a call to action for consumers, businesses, and organizations to reflect on the delicate ecosystem of our ocean's coral reefs.`
+        description: `Coral reefs are essential ocean habitats that provide food, protection, income, and biodiversity 
+        for millions of people and marine species. Help coral reefs by being responsible visitors, using reef-friendly 
+        products, choosing sustainable seafood, reducing pollution and emissions, and spreading awareness. Support coral 
+        reef restoration projects by donating funds to organisations such as coral gardeners.`
     },
     {
         date: "3 June",
         day: "World Bicycle Day",
-        description: `The resolution for World Bicycle Day recognizes "the uniqueness, longevity and versatility of the bicycle, 
-        which has been in use for two centuries, and that it is a simple, affordable, reliable, clean and environmentally fit 
-        sustainable means of transport."`
+        description: `An event that promotes the use of bicycles as a means of sustainable transportation, health, education, 
+        and social inclusion. Celebrate the day by riding your bike, joining a local cycling event, wearing a helmet and reef-friendly 
+        sunscreen, and spreading the word about the importance of bicycles for our planet and our future.`
     },
     {
         date: "5 June",
         day: "World Environment Day",
-        description: `#OnlyOneEarth calls for collective, transformative action on a global scale to celebrate, protect and restore our planet.`
+        description: `It aims to raise awareness and inspire action for the protection and restoration of our natural world. 
+        The day also recognizes the efforts and achievements of individuals, communities, and organizations that are working to 
+        create a greener future. The 2023 celebration will be hosted by Côte d’Ivoire, in partnership with the Netherlands.`
+    },
+    {
+        date: "6 June",
+        day: "World Green Roof Day",
+        description: `Green roofs are important for climate change adaptation, biodiversity, urban greening, and well-being. 
+        Green roofs are nature’s allies in the fight against climate change. They soothe the scorching heat, save money and energy, 
+        soak up the rain, store the carbon, shelter the wildlife, and uplift the mood. `
     },
     {
         date: "8 June",
         day: "World Oceans Day",
-        description: `This year’s celebration will highlight the theme Revitalization: Collective Action for the Ocean. 
-        The ocean connects, sustains, and supports us all. Yet its health is at a tipping point and so is the 
-        well-being of all that depends on it.`
+        description: `A healthy ocean is a critical part of the solution to the climate and biodiversity crises, and protecting at 
+        least 30% of our lands, waters, and ocean by 2030 (30x30) is necessary to give the planet a chance to recover and protect 
+        millions of species from extinction. By supporting this 30×30 project, together we can protect our planet’s life support 
+        systems – specifically the interconnected issues of ocean, climate, and biodiversity.`
+    },
+    {
+        date: "15 June",
+        day: "Nature Photography Day",
+        description: `Nature Photography Day is a fun holiday that celebrates the art and joy of capturing the beauty of nature 
+        through cameras. The day aims to raise awareness of the importance of nature photography for the conservation and protection 
+        of plants, wildlife, and landscapes. The day also encourages people to explore and enjoy nature through their lenses and share 
+        their images with others.`
     },
     {
         date: "16 June",
         day: "World Sea Turtle Day",
-        description: `Celebrated in the birthday of Dr. Archie Carr, 'Father of Sea turtle biology'. 
-        But with 8 million tonnes of plastic dumped in our oceans every year, these amazing creatures 
-        are under threat. In fact, six out of seven species of marine turtle are threatened with extinction.`
+        description: `Sea turtles help maintain the health of seagrass beds and coral reefs that benefit commercially valuable species 
+        such as shrimp, lobster, and tuna. Turtles seem to prefer red, orange and yellow-coloured food. They appear to investigate these 
+        colours more than others when looking for a meal. So reducing marine plastic pollution is critical to saving turtles.`
+    },
+    {
+        date: "16 June",
+        day: "Fresh Veggies Day",
+        description: `Fresh Veggies Day is a day to celebrate the nutritional value and health benefits of fresh vegetables. 
+        It aims to create awareness about the importance of consuming fresh vegetables for metabolism, growth, development, 
+        and disease prevention. The day also encourages people to visit local farmers’ markets, grow their own vegetables, 
+        or try new recipes with fresh veggies`
     },
     {
         date: "17 June",
         day: "World Day to Combat Desertification and Drought",
-        description: `The day offers a chance to recognize that land degradation neutrality is achievable 
-        through problem-solving, strong community involvement and co-operation at all levels.`
+        description: `The theme for 2023 is "Her land. Her rights.". Women face significant barriers in securing land rights, 
+        limiting their ability to thrive and prosper. And when land becomes degraded and water is scarce, women are often the worst 
+        affected. Investing in women’s equal access to land and associated assets is a direct investment in their future and the future of humanity.`
     },
+    {
+        date: "21 June",
+        day: "World Giraffe Day",
+        description: `Giraffes help regulate the growth of plants, disperse seeds, create habitats for other animals, 
+        and provide food for predators and scavengers. Their presence or absence can signal changes in vegetation, climate, 
+        and wildlife populations. They are threatened by habitat loss, civil unrest, illegal hunting, and ecological changes in their natural range. `
+    },
+    {
+        date: "22 June",
+        day: "World Rainforest Day",
+        description: `Rainforests are beneficial for the planet because they are carbon sinks, water factories, biodiversity hotspots, 
+        medicine cabinets, and cultural treasures. Rainforests are threatened by deforestation, poaching, climate change, and fires 
+        that harm their ecosystems and the planet. To save rainforests, we need to use their resources wisely, support ecotourism, 
+        regulate their exploitation, and advocate for their protection.`
+    }
 ]
