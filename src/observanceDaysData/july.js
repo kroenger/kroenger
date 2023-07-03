c72a031d6372649da1b373f4ef307953db280168
Augu <joseyaugustine18@gmail.com>
2023-07-03 16:57:02 +0530

    july observance day update

--- src/observanceDaysData/july.js
+++ src/observanceDaysData/july.js
@@ -1,5 +1,29 @@
 export const july = [
     {
+        date: "3 July",
+        day: "Plastic Bag Free Day",
+        description: `A day to raise awareness about the harmful impacts of single-use plastic bags on our environment, 
+        our wildlife, and our health. Celebrate the day by ditching the plastic and using reusable bags, joining a beach 
+        clean-up campaign, wearing a reusable mask and gloves, and spreading the word about the need to reduce plastic waste 
+        for our planet and our future.`
+    },
+    {
+        date: "4 July",
+        day: "Independence from Meat Day",
+        description: `A day to raise awareness about the impacts of meat consumption on health, animals, and the environment. 
+        It encourages people to try a vegan, vegetarian, or at least flexitarian lifestyle. Also, a day to explore the alternatives 
+        to meat that are available from plant sources. Meat production is a major contributor to greenhouse gas emissions, water scarcity, 
+        land degradation, and biodiversity loss.`
+    },
+    {
+        date: "4 July",
+        day: "International Jackfruit Day",
+        description: `Jackfruit can be eaten raw, cooked, or processed into various products, such as chips, jams, ice creams, 
+        and even meat substitutes. It is rich in vitamins, minerals, antioxidants, and fibre, and has many health benefits. 
+        It also has many uses beyond food, such as its wood, seeds, roots, and leaves. Jackfruit is a sustainable and climate-resilient 
+        crop that can help reduce hunger and poverty in many parts of the world.`
+    },
+    {
         date: "12 July",
         day: "Paper Bag Day",
         description: `Paper bags are 100% biodegradable, reusable, and recyclable. Many paper bags
@@ -9,8 +33,18 @@ export const july = [
     {
         date: "14 July",
         day: "World Chimpanzee Day",
-        description: `July 14, 1960 is the day Dr. Jane Goodall first stepped foot in what is now Gombe Stream National Park, 
-        Goodall called attention to the remarkable chimpanzee and to this day advocates on their behalf.`
+        description: `A day to honour Dr Jane Goodall's legacy and to raise awareness about the threats that chimpanzees face 
+        from habitat loss, poaching, disease, and climate change. It is a day to respect and protect these wonderful animals 
+        and to support the efforts to conserve them and their habitats. Chimpanzees are also vital for the health and diversity 
+        of the forests they live in, as they disperse seeds and create habitats for other animals. `
+    },
+    {
+        date: "14 July",
+        day: "Shark Awareness Day",
+        description: `Sharks are vital for the health and balance of the ocean, as they keep other populations in check 
+        and maintain the food web. They have been around for millions of years, but today they face many threats from human 
+        activities, such as overfishing, bycatch, habitat loss, pollution, and climate change. Many shark species are endangered or 
+        vulnerable to extinction and need our protection and conservation.`
     },
     {
         date: "16 July",
@@ -22,20 +56,24 @@ export const july = [
     },
     {
         date: "26 July",
-        day: "International Day for the Conservation of the Mangrove Ecosystem",
+        day: "Mangrove Ecosystem Conservation Day",
         description: `Mangroves are rare, spectacular and prolific ecosystems on the boundary between land and sea. 
-        These extra ordinary ecosystems contribute to the wellbeing, food security, and protection of coastal communities worldwide.`
+        These extra ordinary ecosystems contribute to the wellbeing, food security, and protection of coastal communities worldwide. 
+        It is estimated that more than a third of the world’s mangroves have been lost in the last 50 years, with serious consequences 
+        for biodiversity, climate, and human security. `
     },
     {
         date: "28 July",
         day: "World Nature Conservation Day",
         description: `In order to keep the world healthy, it is important to preserve the environment 
-        and natural resources. World Nature Conservation Day focuses on saving threatened plants and animals.`
+        and natural resources. World Nature Conservation Day focuses on saving threatened plants and animals. It also encourages 
+        us to adopt sustainable practices and lifestyles that reduce our environmental impact and enhance our harmony with nature.`
     },
     {
         date: "29 July",
         day: "International Tiger Day",
-        description: `The goal of the day is to promote a global system for protecting the natural 
-        habitats of tigers and to raise public awareness and support for tiger conservation issues`
+        description: `The goal of the day is to promote a global system for protecting the natural habitats of tigers and to raise 
+        public awareness and support for tiger conservation issues. They are important for the health and balance of the ecosystems 
+        they live in, as they are apex predators and keystone species.`
     },
 ]
