c51ac29e668778bcb07f01ecffa9599ad098a37a
Augu <joseyaugustine18@gmail.com>
2023-08-13 10:08:39 +0530

    august data update

--- src/observanceDaysData/august.js
+++ src/observanceDaysData/august.js
@@ -8,9 +8,8 @@ export const august = [
     {
         date: "9 August",
         day: "International Day of the World's Indigenous Peoples",
-        description: `Indigenous women are the backbone of indigenous peoples’ communities and 
-        play a crucial role in the preservation and transmission of traditional ancestral knowledge. 
-        They have an integral collective and community role as carers of natural resources and keepers of scientific knowledge`
+        description: `The International Day of the World’s Indigenous Peoples is a day to celebrate the diversity and resilience of the world’s indigenous peoples, 
+        who face many challenges and injustices.  They have an integral collective and community role as carers of natural resources and keepers of scientific knowledge.`
     },
     {
         date: "10 August",
@@ -26,4 +25,34 @@ export const august = [
         African and Asian elephants, and to share knowledge and positive solutions for the better 
         care and management of captive and wild elephants.`
     },
+    {
+        date: "19 August",
+        day: "International Orangutan Day",
+        description: `The day is marked to save species that are on the edge of extinction and to raise awareness about the predicament of this species, 
+        which has progressed from endangered to critically endangered. The day is marked to save species that are on the edge of extinction and to raise 
+        awareness about the predicament of this species, which has progressed from endangered to critically endangered.`
+    },  
+    {
+        date: "20 August",
+        day: "World Mosquito Day",
+        description: `A day that commemorates the scientific discovery and the public health significance of mosquitoes. It is celebrated on August 20 every year, 
+        the date when Sir Ronald Ross found out that malaria is transmitted by Anopheles mosquitoes in 1897. The day also aims to raise awareness and alertness 
+        about the diseases caused by mosquitoes and how to prevent them, as well as to celebrate the achievements of science and medicine in fighting malaria 
+        and other mosquito-borne diseases.`
+    },
+    {
+        date: "22 August",
+        day: "World Plant Milk Day",
+        description: `The day is marked annually to spread awareness among people about the benefits of drinking plant milk, a substitute for dairy milk and is used 
+        to make ice cream. Plant-based milk is lactose-free and the best substitute for cow milk. From soy to oat to cashew to hemp: the diversity of vegan milk continues 
+        to grow in tandem with the global shift towards plant-based eating.`
+    },
+    {
+        date: "30 August",
+        day: "International whale shark day",
+        description: `A day for promoting the study and preservation of the largest fish in the world. They feed on plankton and travel large distances to find enough 
+        food to sustain their huge size, and to reproduce, in fact, the distribution of whale sharks indicates the presence of plankton and the overall health of our oceans. 
+        Their white spotted colouration makes these gentle giants easy to distinguish, and popular with snorkelers and divers at sites where they aggregate off the coast. 
+        Unfortunately, whale sharks are in decline in some areas due to overfishing, bycatch, and habitat destruction.`
+    }
 ]
