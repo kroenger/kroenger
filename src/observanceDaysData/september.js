d5f515c13c29cbfb5482f9625fd25cf227deffd4
Thomas Stanly <105310977+thomasstanly@users.noreply.github.com>
2023-09-03 20:36:33 +0530

    Update september.js

--- src/observanceDaysData/september.js
+++ src/observanceDaysData/september.js
@@ -1,6 +1,6 @@
 export const september = [
     {
-        date: "First Saturday of September",
+        date:"First Saturday of September",
         day: "International Vulture Awareness Day",
         description: `Vultures are ecologically vital and are now the most threatened group of 
         birds in the world; populations of many species are under pressure and some are facing extinction.`
@@ -14,10 +14,61 @@ export const september = [
         in the cooling sector, which contributes to food security.`
     },
     {
+        date:"Third Saturday of September ",
+        day:"International Red Panda Day",
+        description:"Red pandas are native to the eastern Himalayas and southwestern China, where they live in high-altitude temperate forests with bamboo understory. They face threats from deforestation, fragmentation, and degradation of their forest home, as well as from hunting for their fur and body parts, and also vulnerable to diseases and parasites and have low reproductive rates. Help them by donating to organisations such as The Red Panda Network, an organization that works to conserve the red panda and its habitat in Nepal and other countries."
+    },
+    {
+        date:"Third Saturday of September",
+        day:" International Coastal Cleanup Day",
+        description:"A global event that engages volunteers to remove trash from the world’s beaches and waterways. In addition to cleaning beaches and waterways,  volunteers contribute to the world’s largest database on marine debris. Marine debris is one of the greatest threats our ocean faces, but luckily it is an issue with which we can all play a part in the solution."
+    },
+    {
+        date:"Septemeber 17",
+        day:"Septemeber 17",
+        description:"Manta rays are close relatives of all sharks and rays, meaning they have a skeleton made of cartilage rather than bone. Unsustainable and unregulated tourism can drive mantas away from important cleaning and feeding sites through overcrowding and poor in-water behaviour. Climate breakdown threatens to change the distribution and abundance of zooplankton which mantas feed on. "
+    },
+    {
+        date:"September 18",
+        day:"World Water Monitoring Day",
+        description:"The day aims to raise awareness and involvement in protecting water resources around the world by empowering citizens to carry out basic monitoring of their local water bodies. Join the observance day celebration by using a simple test kit to sample local water bodies for temperature, acidity (pH), clarity (turbidity) and dissolved oxygen (DO). "
+    },
+    {
         date: "21 September",
         day: "Zero Emissions Day",
         description: `The goal of zero emission day is to help reduce carbon emissions from the atmosphere
         and give the planet a break. We can use this day as an opportunity to raise awareness
         to minimize our carbon footprint.`
+    },
+    {
+        date:"Fourth Friday of September",
+        day:"Hug a Vegetarian Day",
+        description:"A day to celebrate and appreciate those who have chosen to live a meat-free and animal-product-free lifestyle. Being vegetarian may improve your health by lowering your risk of heart disease, cancer, diabetes, and obesity. It may also help animals and the environment by reducing animal suffering, resource consumption, and greenhouse gas emissions."
+    },
+    {
+        date:"September 22",
+        day:" World Rhino Day",
+        description:"Rhinos are amazing animals that play a vital role in maintaining the health and diversity of their habitats. You can adopt a rhino through WWF or other organizations that work to protect rhinos and their habitats. By adopting a rhino, you can contribute to the funding of anti-poaching patrols, habitat restoration, community engagement, and other conservation efforts."
+    },
+    {
+        date:"Fourth Saturday of September",
+        day:" International Rabbit Day",
+        description:"A global observance dedicated to raising awareness about the well-being and welfare of rabbits, while also highlighting their importance as companion animals and in the wild. Rabbits are prone to viral diseases and can become easy prey for a lot of animals. "
+    },
+    {
+        date:"Fourth Sunday of September",
+        day:"World River Day",
+        description:"Rivers are the arteries of our planet; they are lifelines in the truest sense.  World Rivers Day events include river cleanups, educational outings, community festivals, art projects, and more. You can join the celebration by visiting your local river, using water wisely, choosing eco-friendly products, and spreading the word about river conservation. "
+    },
+    {
+        date:"September 29",
+        day:"International Day of Awareness of Food Loss and Waste",
+        description:"The goal of this day is to raise awareness of the importance of food loss and waste. Food loss and waste have many negative economic and environmental impacts. When food is wasted, the resources that go into producing and making food available to consumers are also wasted. Reducing food loss and waste can improve food security and nutrition, lower greenhouse gas emissions and reduce pressures on natural resources such as land and water."
+    },
+    {
+        date:"September 30",
+        day:"Save the Koala Day",
+        description:"Koalas are one of Australia’s most iconic and beloved animals, but they face threats from habitat loss, climate change, disease, and human activity, contributing to population declines. You can do your part to prevent the unthinkable from happening by adopting a koala, planting a tree, making a donation, or sharing educational resources about koalas and their habitats."
     }
+    
 ]
