1a2e34422eb5ba4435b959d86aea36d4c52974d3
Augu <joseyaugustine18@gmail.com>
2023-11-03 22:01:03 +0530

    november data

--- src/observanceDaysData/november.js
+++ src/observanceDaysData/november.js
@@ -28,52 +28,64 @@ export const november = [
     {
         date: "5 November",
         day: "World Tsunami Awareness Day",
-        description: `Tsunamis are rare events but can be extremely deadly. In the past 100 years, 58 of them have claimed more than 
-        260,000 lives, or an average of 4,600 per disaster, surpassing any other natural hazard. Learn the signs of a potential tsunami, 
-        such as an earthquake, a loud roar from the ocean, or unusual ocean behavior, such as a sudden rise or wall of water or sudden 
-        draining of water showing the ocean floor.`
+        description: `Tsunamis can have long-lasting effects on the environment, economy and society of the affected areas. 
+        It can be prevented or mitigated by adopting various measures, such as building seawalls, breakwaters, mangroves and other coastal barriers, 
+        relocating people and assets away from the shore, enforcing strict building codes and land use regulations, and improving disaster risk governance and coordination.`
     },
     {
         date: "6 November",
         day: "International day for preventing of exploitation of the environment in war and armed conflicts",
-        description: `"We must use all of the tools at our disposal, from dialogue and mediation to preventive diplomacy, 
-        to keep the unsustainable exploitation of natural resources from fueling and financing armed conflict and destabilizing 
-        the fragile foundations of peace." - Ban Ki Moon`
+        description: `The theme for this year’s observance is "The Earth does not belong to us: we belong to the Earth". 
+        The theme invites us to reflect on our relationship with the planet and our responsibility to protect it from harm, 
+        especially in times of war and armed conflict. `
     },
     {
         date: "10 November",
         day: "World Science Day for Peace and Development",
-        description: `The Day highlights the significant role of science in society and the need to engage the wider public 
-        in debates on emerging scientific issues. It also underlines the importance and relevance of science in daily living.`
+        description: `The theme of this year is "Building Trust in Science". This theme highlights the need for enhancing trust in 
+        science among scientists, policymakers, and society. Trust in science is vital for developing and applying evidence-based 
+        solutions to global challenges. Trust in science also affects the way science is conducted and communicated.`
     },
     {
         date: "Third Thursday of November",
         day: "Use Less Stuff Day",
-        description: `Use Less Stuff Day falls on the third Thursday of November every year and was created to spread awareness
-        about the clutter of unnecessary things in our homes. It encourages people to adopt a minimalist lifestyle where only 
-        the essentials are bought or kept.`
+        description: `A day to raise awareness about the environmental impact of our consumption and waste, and to encourage us 
+        to adopt a more minimalist and sustainable lifestyle. Reduce your consumption of disposable items, such as plastic bottles, 
+        paper cups, and plastic bags. Use reusable alternatives instead, such as glass bottles, ceramic mugs, and cloth bags.`
     },
     {
         date: "19 November",
         day: "World Toilet Day",
-        description: `This day is celebrated with the aim of tackling global sanitation crisis and achieve Sustainable 
-        Development Goal 6: water and sanitation for all by 2030. The World Toilet Day 2022 campaign, 
-        called 'Making the invisible visible', focuses on the impact of the sanitation crisis on groundwater, 
-        exploring how inadequate sanitation systems spread human waste into rivers, lakes and soil, polluting underground water resources.`
+        description: `To inspire action to tackle the global sanitation crisis that affects billions of people around the world who are 
+        living without access to a safely managed toilet. The theme for World Toilet Day 2023 is “Accelerating Change” and it encourages 
+        everyone to do something to help solve the problem of inadequate sanitation.        `
     },
     {
         date: "26 November",
         day: "Buy Nothing Day",
-        description: `Buy Nothing Day is a day of protest in which participants pledge to buy nothing for 24 hours 
-        to raise awareness of the negative environmental, social, and political consequences of overconsumption. 
-        It is typically observed in North America on the Friday following Thanksgiving Day in the United States, 
-        which is one of the busiest shopping days of the year.`
+        description: `Buy Nothing Day is a day of protest in which participants pledge to buy nothing for 24 hours to raise awareness 
+        of the negative environmental, social, and political consequences of overconsumption. It is typically observed in North America 
+        on the Friday following Thanksgiving Day in the United States, which is one of the busiest shopping days of the year.`
     },
     {
         date: "27 November",
         day: "Turtle Adoption Day",
-        description: `Different from World Turtle Day, World Turtle Adoption day focuses on the adoption and 
-        care of turtles that are stranded and are in need of love. Just as the theme of World Turtle Day 2022 
-        suggests, we can "SHELLEBRATE" this day and make 'everyone to Love and Save Turtles'.`
+        description: `Different from World Turtle Day, World Turtle Adoption Day focuses on the adoption and care of turtles that are 
+        stranded and are in need of love.  Turtles are reptiles that live in various habitats around the world, but many of them are 
+        threatened by human activities, such as hunting, pollution, and habitat loss.`
     },
+    {
+        date: "29 November",
+        day: "International Jaguar Day",
+        description: `The jaguar is an icon of cultural heritage and sustainable development in Central and South America, where it has 
+        been revered for centuries by indigenous peoples. The jaguar is also a vital part of the ecosystems it inhabits, helping to maintain 
+        the balance of prey and predators, and dispersing seeds of many plants.`
+    },
+    {
+        date: "30 November",
+        day: "International ESG Day",
+        description: `A global initiative that aims to raise awareness and action on environmental, social, and governance (ESG) principles. 
+        The celebration recognizes the efforts of individuals, organizations, and communities who are committed to 
+        shaping a sustainable, ethical, and inclusive future.`
+    }
 ]
