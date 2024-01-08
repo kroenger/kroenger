75f0bb2eb4befcb6f7a1fada4fffd475d7a86107
Augu <joseyaugustine18@gmail.com>
2024-01-08 19:27:43 +0530

    january update

--- src/observanceDaysData/january.js
+++ src/observanceDaysData/january.js
@@ -1,9 +1,27 @@
 export const january = [
     {
+        date: "20 January",
+        day: "Penguin Awareness Day",
+        description: `A day to celebrate and learn more about these amazing aquatic birds. 
+        Penguins are fascinating and adorable creatures, but they are also facing many threats, 
+        such as climate change, overfishing, pollution, and predators. The world’s second-largest 
+        colony of emperor penguins has nearly disappeared after changes in sea/ice conditions made 
+        their breeding ground unstable. The species might lose anywhere between 50% and 70% of its 
+        global population by the end of this century.`
+    },
+    {
         date: "24 January",
         day: "International Day of Education",
         description: `Without inclusive and equitable quality education and lifelong opportunities for all, 
         countries will not succeed in achieving gender equality and breaking the cycle of poverty that is leaving 
         millions of children, youth and adults behind.`
+    },
+    {
+        date: "28 January",
+        day: "International reducing co2 emissions day",
+        description: `This day intends to change the way countries around the world operate by raising public awareness 
+        to the point where governments will support anti-emission measures. Since CO2 is such a big contributor to 
+        climate change, this event seeks to aid the world’s transition to a cleaner environment. The awareness of the 
+        need to reduce greenhouse gas or CO2 emissions has been on the radar of scientists for more than 100 years.`
     }
 ]
