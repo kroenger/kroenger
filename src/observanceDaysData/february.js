97d6664aa7bc2f5caaadad2a5fb69a0b14016586
Augu <joseyaugustine18@gmail.com>
2024-02-06 22:44:47 +0530

    february update

--- src/observanceDaysData/february.js
+++ src/observanceDaysData/february.js
@@ -9,9 +9,18 @@ export const february = [
     {
         date: "10 February",
         day: "World Pulses Day",
-        description: `The celebration aims to raise awareness of the health benefits of consuming pulses, 
-        their role in food security, and the environmental benefits of growing them, such as their ability 
-        to fix nitrogen in the soil, reducing the need for synthetic fertilizers.`
+        description: `Pulses are low in fat and rich in soluble fiber, which can lower cholesterol and 
+        help in the control of blood sugar. The celebration aims to raise awareness of the health benefits 
+        of consuming pulses, their role in food security, and the environmental benefits of growing them, 
+        such as their ability to fix nitrogen in the soil and reduce the need for synthetic fertilizers.`
+    },
+    {
+        date: "15 February",
+        day: "World Hippo Day",
+        description: `Hippos are semiaquatic mammals that are native to sub-Saharan Africa. Hippos are facing 
+        many threats, such as habitat loss, drought, hunting, and poaching. The International Union for Conservation 
+        of Nature (IUCN) listed them as a vulnerable species in 2006 after estimating that their population had 
+        declined by about 20% in the previous two decades.`
     },
     {
         date: "Third Saturday of February",
@@ -21,11 +30,28 @@ export const february = [
         The population is in decline as they are the most trafficked species in the world.`
     },
     {
+        date: "Third Sunday of February",
+        day: "World Whale Day",
+        description: `To celebrate the beauty and diversity of whales, and to raise awareness about 
+        the threats they face from human activities such as pollution, climate change, and whaling. 
+        Whaling is a complex and contentious issue that involves ethical, environmental, and economic 
+        factors. Many people oppose whaling because they believe that whales are intelligent, social, 
+        and endangered animals that deserve protection and respect.`
+    },
+    {
         date: "27 February",
         day: "International Polar Bear Day",
-        description: `The day is celebrated to raise awareness about the challenges facing polar bears, 
-        such as global warming and habitat loss and to 
-        educate people about the importance of protecting polar bears and their habitat and to encourage 
-        people to take action to help conserve these magnificent animals.`
+        description: `To raise awareness about the impact of global warming and reduced sea ice on polar 
+        bear populations. The day was established to coincide with the time period when polar bear moms 
+        and cubs were snug in their dens. On average, only about half of all cubs reach adulthood, with 
+        even lower survival rates in the most vulnerable populations.`
+    },
+    {
+        date: "27 February",
+        day: "World NGO Day",
+        description: `The day aims to inspire people to become more involved with NGOs and their causes, 
+        and to raise awareness of the various challenges and opportunities they face. They help to address 
+        various social, environmental, and humanitarian issues around the world, such as poverty, hunger, 
+        health, education, human rights, disaster relief, and more.`
     }
 ]
