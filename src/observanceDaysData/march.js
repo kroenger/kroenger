3d4ea195b8240c6aa16f67edacfaeb4863a9a9a6
Augu <joseyaugustine18@gmail.com>
2024-03-01 08:24:04 +0530

    march update

--- src/observanceDaysData/march.js
+++ src/observanceDaysData/march.js
@@ -2,34 +2,55 @@ export const march = [
     {
         date: "3 March",
         day: "World Wildlife Day",
-        description: `The focus of World Wildlife Day 2023 is on "Partnerships for Wildlife Conservation". 
-        The goal is to emphasize the significance of working together globally to preserve wildlife. 
-        To safeguard the welfare of wild animals, it is essential for governments to forge partnerships, 
-        team up with civil society groups, and involve local communities in conservation efforts.`
+        description: ` The theme for World Wildlife Day 2024 is Connecting People and Planet: Exploring Digital Innovation in Wildlife Conservation. 
+        This theme highlights the role of digital technologies and innovations in protecting and conserving wildlife and their habitats.`
+    },
+    {
+        date: "7 March",
+        day: "Plant Power Day",
+        description: `A day dedicated to celebrating and promoting the benefits of a plant-based lifestyle. And to raise awareness 
+        about the many benefits of plant-based diets, including their positive impact on human health, animal welfare, and the environment.`
     },
     {
         date: "14 March",
         day: "International Day of Action for Rivers",
-        description: `It is observed to raise awareness about the importance of rivers and advocate for their protection. 
-        The theme for 2023 is "Rights of Rivers," which focuses on recognizing rivers as living entities and promoting their 
-        legal rights. The day is marked by various events and activities around the world, including river cleanups, 
-        educational programs, and advocacy campaigns.`
+        description: `The theme for 2024 is Water for All. This theme focuses on the uneven access to clean water and the pollution 
+        occurring due to human activities. Participate in river cleanups, webinars, walks, or paddleboard events in your area or online.`
+    },
+    {
+        date: "14 March",
+        day: "Science Education Day",
+        description: `Celebrated on the birthday of famous physicist Albert Einstein, the day aims to inspire curiosity, 
+        creativity, and innovation among students and teachers, and to highlight the role of science in solving global challenges. 
+        It is a great opportunity to appreciate the wonders of science and its contributions to humanity.`
+    },
+    {
+        date: "18 March",
+        day: "Global Recycling Day",
+        description: `The theme for 2024 is Recycling Heroes. This theme recognizes the people, places, and activities that 
+        showcase what an important role recycling plays in contributing to a greener future. Watch or listen to some documentaries, 
+        videos, or podcasts that explain the value and challenges of recycling.`
     },
     {
         date: "21 March",
         day: "International Day of Forests",
-        description: `This years celebration will focus on the theme of “Forests and Health,” highlighting the essential role that 
-        forests play in promoting human health through their contribution to clean air and water, food security, medicinal resources, 
-        and economic livelihoods. It will emphasize the urgent need to protect and restore the world's forests, which are under threat 
-        from deforestation, climate change, and other environmental pressures.`
+        description: `The theme for 2024 is Forests and Health. This theme recognizes the linkages between forests and human health, 
+        and how forests can provide food, medicine, clean air, and water, as well as reduce stress and improve well-being. Visit a 
+        forest museum, park, or reserve and learn about the work of forest managers, scientists, and communities.`
     },
     {
         date: "22 March",
         day: "World Water Day",
-        description: `The theme for World Water Day 2023 is “Accelerating Change”. It is about speeding up the progress 
-        towards achieving Sustainable Development Goal (SDG) 6: water and sanitation for all by 2030. The celebration of 
-        World Water Day 2023 coincides with the start of the UN 2023 Water Conference (March 22-24, New York). The conference 
-        is a once-in-a-generation opportunity to unite the world around solving the water and sanitation crisis.`
+        description: `An annual event that aims to raise awareness and take action on the global water crisis. It is held on 22 March 
+        every year since 1993, and each year has a different theme related to water issues. The theme for 2024 is “Leveraging Water for Peace”, 
+        which focuses on how water can be a tool for cooperation and conflict resolution among communities and countries.`
+    },
+    {
+        date: "23 March",
+        day: "World Water Day",
+        description: `A global event that celebrates all eight species of bears and raises awareness about their threats and conservation. 
+        Bears have excellent senses of smell, sight, and hearing. They can smell food, cubs, a mate, or predators from miles away. Their great 
+        eyesight allows them to detect when fruits are ripe.`
     },
     {
         date: "25 March",
