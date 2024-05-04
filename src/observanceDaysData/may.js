32feee8c46e0e22a60ac854ac4827bbc40a3a01b
Augu <joseyaugustine18@gmail.com>
2024-05-04 22:25:06 +0530

    may month update

--- src/observanceDaysData/may.js
+++ src/observanceDaysData/may.js
@@ -12,48 +12,51 @@ export const may = [
     {
         date: "12 May",
         day: "International Day of Plant Health",
-        description: ` The theme for this year is Plant Health for Environmental Protection.
-        Using natural pesticides or other environmentally friendly methods prevents or controls plant pests and 
-        diseases, rather than relying on chemicals that can harm plants, soil, water, and biodiversity. Protecting 
-        plant health is a shared responsibility that requires cooperation and coordination among different actors, 
-        such as farmers, gardeners, researchers, policymakers, traders, consumers, and international organizations.`
+        description: `The theme for the International Day of Plant Health in 2024 focuses on “Plant health, safe trade, and digital technology,” 
+        recognizing the role of innovation in ensuring the health of plants globally. It’s a call to action for everyone to contribute to keeping 
+        our plants healthy, which is essential for food security, sustainable economies, and livelihoods.`
     },
     {
-        date: "13 May",
+        date: "Second Saturday of May",
         day: "World Migratory Bird Day",
-        description: `This year’s theme is "Water: Sustaining Bird Life". The vast majority of migratory 
-        birds rely on aquatic ecosystems during their life cycles. Inland and coastal wetlands, rivers, 
-        lakes, streams, marshes, and ponds are all vital for feeding, drinking, or nesting, and also as 
-        places to rest and refuel during their long journeys. Unfortunately, aquatic ecosystems are becoming 
-        increasingly threatened around the world and so are the migratory birds that depend on them.`
+        description: `This year’s theme is "Protect Insects, Protect Birds". This theme highlights the critical role insects 
+        play in the survival of migratory birds, as they provide essential energy and nutrients, especially during migration and 
+        breeding seasons. The decline in insect populations is a major concern, as it has a direct impact on the migratory bird 
+        species that depend on them.`
     },
     {
         date: "20 May",
         day: "World Bee Day",
-        description: `Bees are essential pollinators for many crops, such as fruits, vegetables, nuts and seeds. Without bees, many crops would have lower yields and quality, 
-        or even fail to produce at all. We can all help by supporting organic and sustainable farming practices, planting bee-friendly plants in our gardens and balconies, 
-        avoiding or minimizing pesticide use, and providing water and shelter for bees.`
+        description: `This year's theme is 'Bee Engaged with Youth'. Bees are essential pollinators for many crops, such as fruits, vegetables, nuts and seeds. Without bees, 
+        many crops would have lower yields and quality, or even fail to produce at all. We can all help by supporting organic and sustainable farming practices, 
+        planting bee-friendly plants in our gardens and balconies, avoiding or minimizing pesticide use, and providing water and shelter for bees.`
     },
     {
         date: "22 May",
         day: "International Day for Biological Diversity",
-        description: `The theme for 2023 is “From Agreement to Action: Build Back Biodiversity” which builds on the results of the 15th Conference of Parties 
-        to the UN Convention on Biological Diversity(COP-15) where the Kunming - Montreal Global Biodiversity Framework was adopted. This global framework plans 
-        to conserve and restore nature by 2030 and beyond. The theme also emphasizes the need to shift from agreement to action and implement the framework in order 
-        to build back biodiversity after the pandemic and other threats.`
+        description: `This year's theme  is "Be part of the Plan". This theme is a call to action for all stakeholders to support the implementation 
+        of the Kunming-Montreal Global Biodiversity Framework, also known as the Biodiversity Plan1. The Biodiversity Plan is a significant global effort 
+        to halt and reverse the loss of biodiversity.`
     },
     {
         date: "23 May",
         day: "World Turtle Day",
-        description: `The theme for 2023 is “I Love Turtles!” which asks everyone to embrace their inner turtle and show their appreciation of these fantastic creatures. 
-        The day educates people about the threats that turtles and tortoises face, such as smuggling, habitat destruction, pet trade, and live exotic food markets. 
+        description: `The day educates people about the threats that turtles and tortoises face, such as smuggling, habitat destruction, pet trade, and live exotic food markets. 
         According to experts, turtles are the most threatened of the major groups of vertebrates, more so than birds, mammals, and fish.`
+    },
+    {
+        date: "25 May",
+        day: "World Fish Migration Day",
+        description: `A global awareness campaign celebrated every two years to highlight the connections that migratory fishes 
+        have with human and non-human species, lands, and waters. This day is marked by celebrations, events, and action campaigns 
+        organized by people around the world to connect fish, rivers, and people. This year's theme is Free Flow. On this day, 
+        we focus on the importance of free-flowing rivers and their impact on migratory fish.`
     },
     {
         date: "31 May",
         day: "World No Tobacco Day",
-        description: `The theme for 2023 is “We need food, not tobacco, " emphasising the importance of healthy food choices and prioritising food over tobacco. 
-        The 2023 campaign also aims to expose the tobacco industry’s efforts to interfere with attempts to substitute tobacco growing with sustainable crops, 
-        thereby contributing to the global food crisis. Tobacco growing and production exacerbates food insecurity, soil degradation, deforestation, and desertification.`
+        description: `This year's theme is "Protecting children from tobacco industry interference". This day is an opportunity to highlight the health risks 
+        associated with tobacco use and advocate for effective policies to reduce tobacco consumption. The focus is on giving a platform to young people who are 
+        calling for the tobacco industry to stop targeting them with harmful products`
     },
 ]
