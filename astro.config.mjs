fed630639e0692b2467b08b37eb808aa0509a7f4
Augu <joseyaugustine18@gmail.com>
2024-11-09 23:15:34 +0530

    feat(tailwind): tailwind init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kroenger.org',
-  integrations: [react(), sitemap()]
+  integrations: [react(), sitemap(), tailwind()]
 });
