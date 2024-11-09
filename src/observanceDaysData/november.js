d29c9d2fdfea32e97f62ce3c270511ca5d7e8170
Augu <joseyaugustine18@gmail.com>
2024-11-09 08:13:43 +0530

    update(november): november data update

--- src/observanceDaysData/november.js
+++ src/observanceDaysData/november.js
@@ -35,57 +35,41 @@ export const november = [
     {
         date: "6 November",
         day: "International day for preventing of exploitation of the environment in war and armed conflicts",
-        description: `The theme for this year’s observance is "The Earth does not belong to us: we belong to the Earth". 
-        The theme invites us to reflect on our relationship with the planet and our responsibility to protect it from harm, 
-        especially in times of war and armed conflict. `
+        description: `Observed on November 6th, this day raises awareness about the environmental damage caused by warfare. It emphasizes the need for stronger laws to protect natural habitats and promotes sustainable peacebuilding efforts that incorporate environmental considerations. Safeguarding our planet is crucial for a healthier, conflict-free future.`
     },
     {
         date: "10 November",
         day: "World Science Day for Peace and Development",
-        description: `The theme of this year is "Building Trust in Science". This theme highlights the need for enhancing trust in 
-        science among scientists, policymakers, and society. Trust in science is vital for developing and applying evidence-based 
-        solutions to global challenges. Trust in science also affects the way science is conducted and communicated.`
+        description: `Celebrated on November 10th, this day highlights the critical role of science in society and the need for international collaboration to tackle global challenges. It promotes scientific research and innovation for sustainable development, peace, and community well-being. Celebrating scientists' contributions inspires future generations to pursue knowledge and positively impact the world.`
     },
     {
         date: "Third Thursday of November",
         day: "Use Less Stuff Day",
-        description: `A day to raise awareness about the environmental impact of our consumption and waste, and to encourage us 
-        to adopt a more minimalist and sustainable lifestyle. Reduce your consumption of disposable items, such as plastic bottles, 
-        paper cups, and plastic bags. Use reusable alternatives instead, such as glass bottles, ceramic mugs, and cloth bags.`
+        description: `A day to raise awareness about the environmental impact of our consumption and waste, and to encourage us to adopt a more minimalist and sustainable lifestyle. Reduce your consumption of disposable items, such as plastic bottles, paper cups, and plastic bags. Use reusable alternatives instead, such as glass bottles, ceramic mugs, and cloth bags.`
     },
     {
         date: "19 November",
         day: "World Toilet Day",
-        description: `To inspire action to tackle the global sanitation crisis that affects billions of people around the world who are 
-        living without access to a safely managed toilet. The theme for World Toilet Day 2023 is “Accelerating Change” and it encourages 
-        everyone to do something to help solve the problem of inadequate sanitation.        `
+        description: `To inspire action to tackle the global sanitation crisis that affects billions of people around the world who are living without access to a safely managed toilet. The theme for World Toilet Day 2023 is “Accelerating Change” and it encourages everyone to do something to help solve the problem of inadequate sanitation.        `
     },
     {
         date: "26 November",
         day: "Buy Nothing Day",
-        description: `Buy Nothing Day is a day of protest in which participants pledge to buy nothing for 24 hours to raise awareness 
-        of the negative environmental, social, and political consequences of overconsumption. It is typically observed in North America 
-        on the Friday following Thanksgiving Day in the United States, which is one of the busiest shopping days of the year.`
+        description: `Buy Nothing Day is a day of protest in which participants pledge to buy nothing for 24 hours to raise awareness of the negative environmental, social, and political consequences of overconsumption. It is typically observed in North America on the Friday following Thanksgiving Day in the United States, which is one of the busiest shopping days of the year.`
     },
     {
         date: "27 November",
         day: "Turtle Adoption Day",
-        description: `Different from World Turtle Day, World Turtle Adoption Day focuses on the adoption and care of turtles that are 
-        stranded and are in need of love.  Turtles are reptiles that live in various habitats around the world, but many of them are 
-        threatened by human activities, such as hunting, pollution, and habitat loss.`
+        description: `Different from World Turtle Day, World Turtle Adoption Day focuses on the adoption and care of turtles that are stranded and are in need of love.  Turtles are reptiles that live in various habitats around the world, but many of them are threatened by human activities, such as hunting, pollution, and habitat loss.`
     },
     {
         date: "29 November",
         day: "International Jaguar Day",
-        description: `The jaguar is an icon of cultural heritage and sustainable development in Central and South America, where it has 
-        been revered for centuries by indigenous peoples. The jaguar is also a vital part of the ecosystems it inhabits, helping to maintain 
-        the balance of prey and predators, and dispersing seeds of many plants.`
+        description: `The jaguar is an icon of cultural heritage and sustainable development in Central and South America, where it has been revered for centuries by indigenous peoples. The jaguar is also a vital part of the ecosystems it inhabits, helping to maintain the balance of prey and predators, and dispersing seeds of many plants.`
     },
     {
         date: "30 November",
         day: "International ESG Day",
-        description: `A global initiative that aims to raise awareness and action on environmental, social, and governance (ESG) principles. 
-        The celebration recognizes the efforts of individuals, organizations, and communities who are committed to 
-        shaping a sustainable, ethical, and inclusive future.`
+        description: `A global initiative that aims to raise awareness and action on environmental, social, and governance (ESG) principles. The celebration recognizes the efforts of individuals, organizations, and communities who are committed to shaping a sustainable, ethical, and inclusive future.`
     }
 ]
