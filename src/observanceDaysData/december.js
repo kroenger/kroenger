584f99a8d29f3177e798d801e36cd8a51c392700
Augu <joseyaugustine18@gmail.com>
2024-11-24 14:18:42 +0530

    december update

--- src/observanceDaysData/december.js
+++ src/observanceDaysData/december.js
@@ -2,72 +2,46 @@ export const december = [
     {
         date: "3 December",
         day: "World Coati Day",
-        description: `An annual event that aims to raise awareness and conservation efforts for the coatimundi, a relative of the raccoon 
-        that lives in Central and South America. It is celebrated on December 3rd, which is in the middle of the coati breeding season. 
-        World Coati Day was founded by Rhianna Wilton, a wildlife specialist and zookeeper from Australia, who wanted to share the beauty 
-        and facts about these unique and underappreciated animals.`
+        description: `Celebrated on December 3rd, this day raises awareness and promotes conservation efforts for the coatimundi, a relative of the raccoon found in Central and South America. Founded by Australian wildlife specialist and zookeeper Rhianna Wilton, it highlights the beauty and significance of these unique, underappreciated animals, coinciding with the middle of the coati breeding season.`
     },
     {
         date: "4 December",
         day: "International Cheetah Day",
-        description: `A day to celebrate and raise awareness about the world’s fastest land animal, the cheetah. It was founded by 
-        Dr. Laurie Marker, an American zoologist who dedicated her life to cheetah conservation in Namibia. The date of December 
-        4th was chosen in honour of Khayam, a cheetah cub that Dr. Marker raised and took to Namibia for a wildlife experiment. 
-        The day aims to educate people about the threats that cheetahs face and how to protect them from extinction.`
+        description: `Celebrated on December 4th, this day honors the world's fastest land animal, the cheetah. Founded by Dr. Laurie Marker, an American zoologist dedicated to cheetah conservation in Namibia, it commemorates Khayam, a cheetah cub she raised and brought to Namibia for a wildlife experiment. The day aims to educate about the threats cheetahs face and how to protect them from extinction.`
     },
     {
         date: "4 December",
         day: "World Wildlife Conservation Day",
-        description: `An annual event that aims to raise awareness and take action to protect the planet’s wildlife biodiversity. It is 
-        celebrated on December 4 and was initiated by the United States in 2012 to combat the illegal trade of endangered species. Some 
-        of the most threatened wildlife species include tigers, pandas, gorillas, rhinos, and orangutans. They face various challenges 
-        such as habitat loss, poaching, hunting, and human-wildlife conflict. Conservation efforts are needed to ensure their survival 
-        and the health of the ecosystems they inhabit.`
+        description: `Celebrated on December 4th, this event raises awareness and promotes action to protect wildlife biodiversity. Initiated by the United States in 2012, it combats the illegal trade of endangered species. Threatened wildlife, including tigers, pandas, gorillas, rhinos, and orangutans, face habitat loss, poaching, and human-wildlife conflict. Conservation efforts are crucial to ensure their survival and the health of ecosystems.`
     },
     {
         date: "5 December",
         day: "World Soil Day",
-        description: `An annual observance that aims to raise awareness of the importance of healthy soil and sustainable soil management 
-        practices. It is celebrated on December 5, which coincides with the birthday of the late King Bhumibol of Thailand, who was a 
-        champion of soil conservation. The theme for World Soil Day 2023 is “Soil and water, a source of life”, which highlights the 
-        vital link between soil and water for food security, ecosystem services, and human well-being.`
+        description: `An annual observance that aims to raise awareness of the importance of healthy soil and sustainable soil management practices. Celebrated on December 5th, which coincides with the birthday of the late King Bhumibol of Thailand, a champion of soil conservation, the theme for World Soil Day 2024 is “Caring for Soils: Measure, Monitor, Manage.” This theme highlights the vital link between soil and water for food security, ecosystem services, and human well-being.`
     },
     {
         date: "9 December",
         day: "International Day of Veterinary Medicine",
-        description: `A day to celebrate and appreciate the work of veterinarians and veterinary organizations around the world. It is 
-        observed on December 9th every year, which marks the anniversary of the founding of the first veterinary school in Lyon, 
-        France in 1762. The day also highlights the importance of animal health and welfare for human health and the environment.`
+        description: `Celebrated on December 9th, this day honors the essential work of veterinarians and veterinary organizations worldwide. It marks the anniversary of the first veterinary school founded in Lyon, France in 1762. This observance highlights the vital role of animal health and welfare in ensuring human health and environmental balance. Veterinarians prevent and control diseases, conduct life-saving surgeries, and promote overall animal well-being, benefiting all living beings.`
     },
     {
         date: "10 December",
         day: "International Animal Rights Day",
-        description: `An annual event that honours animals as sentient beings who deserve the same protections as humans. It is observed 
-        on December 10 every year, coinciding with Human Rights Day. The event aims to raise awareness of animal exploitation, mourn the 
-        animal victims of human tyranny, and rally support for universal animal welfare.`
+        description: `Observed on December 10th, this annual event honors animals as sentient beings deserving the same protections as humans. Coinciding with Human Rights Day, it aims to raise awareness of animal exploitation, mourn animal victims of human tyranny, and rally support for universal animal welfare. It encourages us to reflect on our relationship with animals and advocate for their rights and protection.`
     },
     {
         date: "11 December",
         day: "International Mountain Day",
-        description: `Celebrated annually on December 11 to raise awareness about the importance of mountains to both life and climate. 
-        The theme of this year’s International Mountain Day is Restoring Mountain Ecosystems. This theme was selected to fully include 
-        mountains in the UN Decade on Ecosystem Restoration 2021–2030, co-led by the Food and Agriculture Organization of the UN and 
-        the UN Environment Programme.`
+        description: `Celebrated annually on December 11th, this day raises awareness about the importance of mountains to both life and climate. The theme for this year’s International Mountain Day is "Mountain solutions for a sustainable future – innovation, adaptation, youth and beyond." This theme focuses on innovative solutions, fostering adaptation strategies, and empowering youth for a sustainable future.`
     },
     {
         date: "14 December",
         day: "Sustainable Entrepreneurship Day",
-        description: `The day aims to promote the importance of sustainability in entrepreneurship. Sustainable entrepreneurship 
-        refers to the practice of creating businesses that not only generate profits but also consider social, environmental, and 
-        economic factors. The theme for the day focuses on “Ecosystems for Entrepreneurship”.`
+        description: `The day aims to promote the importance of sustainability in entrepreneurship. Sustainable entrepreneurship refers to creating businesses that generate profits while considering social, environmental, and economic factors. The theme for 2024 is “Innovative, Inclusive, and Impactful,” highlighting the need for innovative solutions, inclusive practices, and impactful actions to foster a sustainable and equitable future for all.`
     },
     {
         date: "27 December",
         day: "The International Day of Epidemic Preparedness",
-        description: `A global observance that aims to raise awareness and action to prevent, prepare for, and respond to epidemics. 
-        The day is observed to stress the significance of partnership and solidarity among individuals, communities, and states in all 
-        stages of epidemic management, to recognize the primary role and responsibility of governments and the indispensable contribution 
-        of relevant stakeholders, especially women, who make up the majority of the world’s health workers, and to promote the exchange 
-        of information, scientific knowledge, and best practices on epidemics at the local, national, regional, and global levels.`
+        description: `A global observance that raises awareness and action to prevent, prepare for, and respond to epidemics. Observed on December 27th, it emphasizes partnership and solidarity among individuals, communities, and states in all stages of epidemic management. It recognizes the primary role of governments and the indispensable contributions of stakeholders, especially women, who constitute the majority of health workers. It also promotes the exchange of information, scientific knowledge, and best practices on epidemics at local, national, regional, and global levels.`
     }
 ]
