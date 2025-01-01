2ad096777425d74e1e6d98205a1bc73906e39ae9
Augu <joseyaugustine18@gmail.com>
2025-01-01 22:06:57 +0530

    feat: add observance days for International Day of Clean Energy and International Zebra Day

--- src/observanceDaysData/january.js
+++ src/observanceDaysData/january.js
@@ -17,11 +17,25 @@ export const january = [
         millions of children, youth and adults behind.`
     },
     {
+        date: "26 January",
+        day: "International Day of Clean Energy",
+        description: `A day to raise awareness about the importance of clean
+        energy and to promote the use of renewable energy sources. Clean energy is energy that is produced from
+        renewable resources, such as sunlight, wind, and water. Clean energy is important because it helps to reduce
+        pollution and greenhouse gas emissions, which are major contributors to climate change.`
+    },
+    {
         date: "28 January",
         day: "International reducing co2 emissions day",
         description: `This day intends to change the way countries around the world operate by raising public awareness 
         to the point where governments will support anti-emission measures. Since CO2 is such a big contributor to 
         climate change, this event seeks to aid the world’s transition to a cleaner environment. The awareness of the 
         need to reduce greenhouse gas or CO2 emissions has been on the radar of scientists for more than 100 years.`
+    },{
+        date: "31 January",
+        day: "International Zebra Day",
+        description: `A day to celebrate and learn more about these amazing striped equines. Zebras are iconic and striking creatures, 
+        but they also face numerous threats, such as habitat loss, poaching, and climate change. The Grévy's zebra, in particular, 
+        has experienced a significant decline in population due to habitat fragmentation and human activities.`
     }
 ]
