09376498657c206639a4a206e49f363b164ad2b0
Augu <joseyaugustine18@gmail.com>
2025-01-10 17:23:01 +0530

    feat: add getImageUrls utility to dynamically generate image URLs for sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config';
+import { getImageUrls } from './utils/getImageUrls';
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kroenger.org',
-  integrations: [react(), sitemap(), tailwind()]
+  integrations: [
+    react(), 
+    sitemap({
+      customPages: [
+        ...getImageUrls('https://kroenger.org')
+      ]
+    }), 
+    tailwind()
+  ]
 });
