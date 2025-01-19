53a6c8e123fba3fa691bd5a8dbdd2a94ae0bdc6c
Augu <joseyaugustine18@gmail.com>
2025-01-19 08:22:15 +0530

    feat: add observance days for International Day of Arabian Leopard and World Bonobo Day; update description for World Wetlands Day and Kyoto Protocol Day

--- src/observanceDaysData/february.js
+++ src/observanceDaysData/february.js
@@ -2,9 +2,9 @@ export const february = [
     {
         date: "2 February",
         day: "World Wetlands Day",
-        description: `A call to take action for wetlands is the focus of this years’ campaign. 
-        It's an appeal to invest financial, human and political capital to save the world’s 
-        wetlands from disappearing and to restore those we have degraded`,
+        description: `Wetlands are crucial ecosystems that provide habitat for a diverse range of species, help regulate water cycles, 
+        and support human livelihoods. This year's theme is "Protecting Wetlands for Our Common Future," emphasizing the urgent need to 
+        conserve and restore these vital areas for biodiversity, climate resilience, and sustainable development.`,
         links:[
             {
                 label: "worldwetlandsday.org",
@@ -27,6 +27,31 @@ export const february = [
         ]
     },
     {
+        date: "10 February",
+        day: "International Day of Arabian Leopard",
+        description: `These critically endangered, elusive creatures are among the smallest leopard subspecies, 
+        yet they are crucial in maintaining the region's ecological balance. They face numerous threats like habitat loss, 
+        poaching, and human-wildlife conflict, making conservation efforts vital.`,
+        links:[]
+    },
+    {
+        date: "14 February",
+        day: "World Bonobo Day",
+        description: `These peaceful apes, often referred to as “hippie chimps,” live in matriarchal societies where cooperation 
+        and social bonds are strong. Critically endangered, bonobos are threatened by habitat loss, poaching, and the illegal pet trade. 
+        Learning from bonobos' social behavior, can inspire us to build more compassionate and cooperative human societies.`,
+        links:[
+            {
+                label: "bonobohandshake.com",
+                value: "https://www.bonobohandshake.com/"
+            },
+            {
+                label: "bonobos.org",
+                value: "https://www.bonobos.org/"
+            }
+        ]
+    },
+    {
         date: "15 February",
         day: "World Hippo Day",
         description: `Hippos are semiaquatic mammals that are native to sub-Saharan Africa. Hippos are facing 
@@ -36,11 +61,25 @@ export const february = [
         links:[]
     },
     {
+        date: "16 February",
+        day: "Kyoto Protocol Day",
+        description: `Adopted on 11 December 1997, this landmark international treaty aims to reduce greenhouse gas emissions 
+        and combat climate change by setting binding emission reduction targets for 37 industrialized countries and economies in transition.
+        The protocol, which took effect on 16 February 2005, is grounded in the principle of "common but differentiated responsibilities".`,
+        links:[
+            {
+                label: "unfccc.int",
+                value: "https://unfccc.int/process-and-meetings/the-kyoto-protocol/what-is-the-kyoto-protocol"
+            }
+        ]
+    },
+    {
         date: "Third Saturday of February",
         day: "World Pangolin Day",
         description: `Pangolins are hunted down extensively for their scales are unfortunately 
         believed to be a cure-all of sorts and pangolin flesh is considered a delicacy. 
-        The population is in decline as they are the most trafficked species in the world.`,
+        These unique creatures are critically endangered due to illegal poaching and trafficking 
+        driven by demand for their scales and meat.`,
         links:[
             {
                 label: "worldpangolinday.org",
