7a4c55d57b8e9062c607beb45d34dab1e59be413
Augu <joseyaugustine18@gmail.com>
2025-02-01 15:09:02 +0530

    feat: configure Vercel adapter and set output to server in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,18 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kroenger.org',
+
   integrations: [
     react(), 
     sitemap(), 
     tailwind()
-  ]
+  ],
+
+  output: "server",
+  adapter: vercel()
 });
