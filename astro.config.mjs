90f762ad5ef9a9e014b934c9d80d95eb7d06fe76
Augu <joseyaugustine18@gmail.com>
2025-02-26 21:54:19 +0530

    chore: update sitemap baseurl

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kroenger.org',
+  site: 'https://www.kroenger.org',
 
   integrations: [
     react(), 
