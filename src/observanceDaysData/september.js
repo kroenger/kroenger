3a568edaf7eb6acd2c743fbf543b2d94ba220d82
Augu <joseyaugustine18@gmail.com>
2025-09-06 17:36:14 +0530

    feat(observance-days): add links for various observance days in September

--- src/observanceDaysData/september.js
+++ src/observanceDaysData/september.js
@@ -6,15 +6,20 @@ export const september = [
         awareness about their endangerment. They are essential for seed dispersal, which helps maintain forest biodiversity. Their feeding 
         habits contribute to the growth and regeneration of tropical forests. The presence and health of primate populations can indicate 
         the overall health of their habitats.`,
-        links:[]
+        links: []
     },
     {
         date: "First Saturday of September",
         day: "International Vulture Awareness Day",
         description: `Vultures are ecologically vital and are now the most threatened group of birds in the world; populations of many species are under 
         pressure and some are facing extinction. Vultures are often misunderstood, but they are essential for maintaining healthy environments by consuming 
-        carcasses and preventing the spread of diseases1. Unfortunately, vulture populations are declining globally due to poisoning, habitat loss, and other threats`,
-        links:[]
+        carcasses and preventing the spread of diseases. Unfortunately, vulture populations are declining globally due to poisoning, habitat loss, and other threats`,
+        links: [
+            {
+                label: "vultureday.org",
+                value: "https://vultureday.org/"
+            }
+        ]
     },
     {
         date: "2 September",
@@ -23,7 +28,20 @@ export const september = [
         coconut farming supports the livelihoods of millions of people in tropical regions, helping communities become more resilient to the economic 
         impacts of climate change. Coconut-based ecosystems can help mitigate climate change by sequestering carbon. Intercropping coconuts with other 
         plants can enhance carbon storage and improve the overall resilience of the ecosystem.`,
-        links:[]
+        links: []
+    },
+    {
+        date: "6 September",
+        day: "World Shorebirds Day",
+        description: `Shorebirds, also known as waders, are a diverse group of birds that inhabit coastal and inland wetlands. They play a crucial role in 
+        maintaining the health of these ecosystems by controlling insect populations, aerating the soil, and contributing to nutrient cycling. Many shorebird
+        species are migratory, traveling long distances between breeding and wintering grounds. Protecting their habitats is essential for their survival.`,
+        links: [
+            {
+                label: "worldshorebirdsday.org",
+                value: "https://www.worldshorebirdsday.org"
+            }
+        ]
     },
     {
         date: "7 September",
@@ -31,7 +49,7 @@ export const september = [
         description: `Air pollution is responsible for millions of premature deaths annually and has significant impacts on climate change, economic losses, and agricultural productivity.
         Pollutants like black carbon and methane contribute to global warming. They alter weather patterns and increase the frequency of extreme weather events.
         Efforts to combat air pollution include international agreements like the Paris Agreement, national policies to reduce emissions, and local initiatives to promote clean energy and sustainable practices.`,
-        links:[]
+        links: []
     },
     {
         date: "12 September",
@@ -40,7 +58,7 @@ export const september = [
         when 1,428 dolphins were killed in the Faroe Islands. Dolphins are considered indicator species, meaning their health reflects the overall health of the 
         marine environment. Monitoring dolphin populations can provide insights into the state of marine ecosystems. As apex predators, dolphins help regulate the 
         populations of fish and squid, ensuring a balanced food web.`,
-        links:[]
+        links: []
     },
     {
         date: "16 September",
@@ -48,7 +66,7 @@ export const september = [
         description: `The ozone layer absorbs solar ultraviolet radiation, which heats the stratosphere. Ozone depletion leads to cooling of the stratosphere but can 
         contribute to warming in the troposphere (the lower part of the atmosphere) due to increased greenhouse gases. Ozone depletion has a negative radiative forcing 
         effect, meaning it tends to cool the Earth’s surface`,
-        links:[]
+        links: []
     },
     {
         date: "Third Saturday of September ",
@@ -57,15 +75,25 @@ export const september = [
         They face threats from deforestation, fragmentation, and degradation of their forest home, as well as from hunting for their fur and body parts, 
         and also vulnerable to diseases and parasites and have low reproductive rates. Help them by donating to organisations such as The Red Panda Network, 
         an organization that works to conserve the red panda and its habitat in Nepal and other countries.`,
-        links:[]
+        links: [
+            {
+                label: "redpandanetwork.org",
+                value: "https://redpandanetwork.org"
+            }
+        ]
     },
     {
         date: "Third Saturday of September",
-        day: " International Coastal Cleanup Day",
+        day: "International Coastal Cleanup Day",
         description: `A global event that engages volunteers to remove trash from the world’s beaches and waterways. In addition to cleaning beaches and waterways, 
         volunteers contribute to the world’s largest database on marine debris. Marine debris is one of the greatest threats our ocean faces, but luckily it is an 
         issue with which we can all play a part in the solution.`,
-        links:[]
+        links: [
+            {
+                label: "oceanconservancy.org",
+                value: "https://oceanconservancy.org/trash-free-seas/international-coastal-cleanup/"
+            }
+        ]
     },
     {
         date: "Septemeber 17",
@@ -73,7 +101,12 @@ export const september = [
         description: `Manta rays are close relatives of all sharks and rays, meaning they have a skeleton made of cartilage rather than bone. Unsustainable and 
         unregulated tourism can drive mantas away from important cleaning and feeding sites through overcrowding and poor in-water behaviour. Climate breakdown 
         threatens to change the distribution and abundance of zooplankton which mantas feed on. `,
-        links:[]
+        links: [
+            {
+                label: "worldmantaday.com",
+                value: "https://www.worldmantaday.com"
+            }
+        ]
     },
     {
         date: "September 18",
@@ -81,7 +114,7 @@ export const september = [
         description: `The day aims to raise awareness and involvement in protecting water resources around the world by empowering citizens to carry out basic 
         monitoring of their local water bodies. Join the observance day celebration by using a simple test kit to sample local water bodies for temperature, acidity (pH), 
         clarity (turbidity) and dissolved oxygen (DO). `,
-        links:[]
+        links: []
     },
     {
         date: "September 18",
@@ -89,7 +122,12 @@ export const september = [
         description: `Bamboo is not only a versatile and fast-growing plant but also plays a crucial role in environmental conservation, economic development,  
         and cultural significance. Bamboo’s fast growth rate allows it to sequester carbon efficiently. It can absorb carbon dioxide from the atmosphere at rates  
         comparable to, or even higher than, many tree species. For instance, one hectare of bamboo can sequester about 17 tonnes of carbon annually.`,
-        links:[]
+        links: [
+            {
+                label: "worldbamboo.net",
+                value: "https://www.worldbamboo.net"
+            }
+        ]
     },
     {
         date: "21 September",
@@ -97,15 +135,7 @@ export const september = [
         description: `Zero emissions play a crucial role in combating climate change. Achieving zero emissions means that no net greenhouse gases are added to the atmosphere. 
         This can be done by reducing emissions as much as possible and balancing any remaining emissions with actions that remove carbon from the atmosphere, such as planting 
         trees or using carbon capture technologies`,
-        links:[]
-    },
-    {
-        date: "21 September",
-        day: "International Coastal Cleanup Day",
-        description: `Marine debris, often referred to as marine litter, has significant and far-reaching impacts on the environment, wildlife, and even human health.
-        Many marine animals, including fish, birds, and mammals, mistake debris for food. Ingesting plastic can lead to malnutrition, intestinal blockage, and even death.
-        Microplastics, tiny plastic particles resulting from the breakdown of larger debris, can enter the food chain and potentially pose health risks to humans.`,
-        links:[]
+        links: []
     },
     {
         date: "Fourth Friday of September",
@@ -113,22 +143,32 @@ export const september = [
         description: `A day to celebrate and appreciate those who have chosen to live a meat-free and animal-product-free lifestyle. Being vegetarian may improve your health by 
         lowering your risk of heart disease, cancer, diabetes, and obesity. It may also help animals and the environment by reducing animal suffering, resource consumption, and 
         greenhouse gas emissions.`,
-        links:[]
+        links: []
     },
     {
         date: "September 22",
-        day: " World Rhino Day",
+        day: "World Rhino Day",
         description: `Rhinos are amazing animals that play a vital role in maintaining the health and diversity of their habitats. You can adopt a rhino through WWF or other 
         organizations that work to protect rhinos and their habitats. By adopting a rhino, you can contribute to the funding of anti-poaching patrols, habitat restoration, 
         community engagement, and other conservation efforts.`,
-        links:[]
+        links: [
+            {
+                label: "worldrhinoday.org",
+                value: "https://www.worldrhinoday.org"
+            }
+        ]
     },
     {
         date: "Fourth Saturday of September",
-        day: " International Rabbit Day",
+        day: "International Rabbit Day",
         description: `A global observance dedicated to raising awareness about the well-being and welfare of rabbits, while also highlighting their importance as companion 
         animals and in the wild. Rabbits are prone to viral diseases and can become easy prey for a lot of animals. `,
-        links:[]
+        links: [
+            {
+                label: "rabbit.org",
+                value: "https://rabbit.org"
+            }
+        ]
     },
     {
         date: "Fourth Sunday of September",
@@ -136,7 +176,12 @@ export const september = [
         description: `Rivers are the arteries of our planet; they are lifelines in the truest sense.  World Rivers Day events include river cleanups, educational outings, 
         community festivals, art projects, and more. You can join the celebration by visiting your local river, using water wisely, choosing eco-friendly products, and 
         spreading the word about river conservation. `,
-        links:[]
+        links: [
+            {
+                label: "worldriversday.com",
+                value: "https://www.worldriversday.com"
+            }
+        ]
     },
     {
         date: "September 29",
@@ -144,7 +189,7 @@ export const september = [
         description: `The goal of this day is to raise awareness of the importance of food loss and waste. Food loss and waste have many negative economic and environmental 
         impacts. When food is wasted, the resources that go into producing and making food available to consumers are also wasted. Reducing food loss and waste can improve 
         food security and nutrition, lower greenhouse gas emissions and reduce pressures on natural resources such as land and water.`,
-        links:[]
+        links: []
     },
     {
         date: "September 30",
@@ -152,7 +197,12 @@ export const september = [
         description: `Koalas are one of Australia’s most iconic and beloved animals, but they face threats from habitat loss, climate change, disease, and human activity, 
         contributing to population declines. You can do your part to prevent the unthinkable from happening by adopting a koala, planting a tree, making a donation, or 
         sharing educational resources about koalas and their habitats`,
-        links:[]
+        links: [
+            {
+                label: "savethekoala.com",
+                value: "https://www.savethekoala.com"
+            }
+        ]
     }
 
 ]
