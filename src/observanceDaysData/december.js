3de430ee160ba50dc56165de41fc1202c2dacc42
Augu <joseyaugustine18@gmail.com>
2025-12-01 11:11:20 +0530

    feat(december): add links for observance days and enhance descriptions

--- src/observanceDaysData/december.js
+++ src/observanceDaysData/december.js
@@ -14,7 +14,12 @@ export const december = [
         description: `Celebrated on December 3rd, this day raises awareness and promotes conservation efforts for the coatimundi, a relative of the raccoon found in 
         Central and South America. Founded by Australian wildlife specialist and zookeeper Rhianna Wilton, it highlights the beauty and significance of these unique, 
         underappreciated animals, coinciding with the middle of the coati breeding season.`,
-        links:[]
+        links:[
+            {
+                label: "worldcoatiday.org",
+                value: "https://worldcoatiday.org"
+            }
+        ]
     },
     {
         date: "4 December",
@@ -22,7 +27,16 @@ export const december = [
         description: `Celebrated on December 4th, this day honors the world's fastest land animal, the cheetah. Founded by Dr. Laurie Marker, an American zoologist 
         dedicated to cheetah conservation in Namibia, it commemorates Khayam, a cheetah cub she raised and brought to Namibia for a wildlife experiment. The day aims 
         to educate about the threats cheetahs face and how to protect them from extinction.`,
-        links:[]
+        links:[
+            {
+                label: "cheetah.org",
+                value: "https://cheetah.org/about/what-we-do/international-cheetah-day/"
+            },
+            {
+                label: "internationalcheetahday.com",
+                value: "https://internationalcheetahday.com/"
+            }
+        ]
     },
     {
         date: "4 December",
@@ -30,14 +44,23 @@ export const december = [
         description: `Celebrated on December 4th, this event raises awareness and promotes action to protect wildlife biodiversity. Initiated by the United States in 
         2012, it combats the illegal trade of endangered species. Threatened wildlife, including tigers, pandas, gorillas, rhinos, and orangutans, face habitat loss, 
         poaching, and human-wildlife conflict. Conservation efforts are crucial to ensure their survival and the health of ecosystems.`,
-        links:[]
+        links:[
+            {
+                label: "wildlifeday.org",
+                value: "https://wildlifeday.org/en"
+            },
+            {
+                label: "iapwa.org",
+                value: "https://iapwa.org/world-wildlife-conservation-day/"
+            }
+        ]
     },
     {
         date: "5 December",
         day: "World Soil Day",
         description: `An annual observance that aims to raise awareness of the importance of healthy soil and sustainable soil management practices. Celebrated on 
-        December 5th, which coincides with the birthday of the late King Bhumibol of Thailand, a champion of soil conservation, the theme for World Soil Day 2024 
-        is “Caring for Soils: Measure, Monitor, Manage.” This theme highlights the vital link between soil and water for food security, ecosystem services, and human well-being.`,
+        December 5th, which coincides with the birthday of the late King Bhumibol of Thailand, a champion of soil conservation. Sustainable soil management practices, 
+        reduce erosion and pollution, and enhance water infiltration and storage.`,
         links:[]
     },
     {
@@ -46,7 +69,16 @@ export const december = [
         description: `Celebrated on December 9th, this day honors the essential work of veterinarians and veterinary organizations worldwide. It marks the anniversary 
         of the first veterinary school founded in Lyon, France in 1762. This observance highlights the vital role of animal health and welfare in ensuring human health 
         and environmental balance. Veterinarians prevent and control diseases, conduct life-saving surgeries, and promote overall animal well-being, benefiting all living beings.`,
-        links:[]
+        links:[
+            {
+                label: "worldvet.org",
+                value: "https://www.worldvet.org"
+            },
+            {
+                label: "ivo.vet",
+                value: "https://www.ivo.vet/"
+            }
+        ]
     },
     {
         date: "10 December",
@@ -54,22 +86,31 @@ export const december = [
         description: `Observed on December 10th, this annual event honors animals as sentient beings deserving the same protections as humans. Coinciding with 
         Human Rights Day, it aims to raise awareness of animal exploitation, mourn animal victims of human tyranny, and rally support for universal animal welfare. 
         It encourages us to reflect on our relationship with animals and advocate for their rights and protection.`,
-        links:[]
+        links:[
+            {
+                label: "worldanimalprotection.org",
+                value: "https://www.worldanimalprotection.org/"
+            },
+            {
+                label: "animal-ethics.org",
+                value: "https://www.animal-ethics.org/international-animal-rights-day/"
+            }
+        ]
     },
     {
         date: "11 December",
         day: "International Mountain Day",
-        description: `Celebrated annually on December 11th, this day raises awareness about the importance of mountains to both life and climate. The theme for 
-        this year’s International Mountain Day is "Mountain solutions for a sustainable future – innovation, adaptation, youth and beyond." This theme focuses on 
-        innovative solutions, fostering adaptation strategies, and empowering youth for a sustainable future.`,
+        description: `Celebrated annually on December 11th, this day raises awareness about the importance of mountains to both life and climate. Melting glaciers and thawing 
+        permafrost increase risks such as floods, glacier lake outburst floods, landslides or enhanced erosion and sediment, endangering downstream populations and critical 
+        infrastructure.`,
         links:[]
     },
     {
         date: "14 December",
         day: "Sustainable Entrepreneurship Day",
         description: `The day aims to promote the importance of sustainability in entrepreneurship. Sustainable entrepreneurship refers to creating businesses 
-        that generate profits while considering social, environmental, and economic factors. The theme for 2024 is “Innovative, Inclusive, and Impactful,” 
-        highlighting the need for innovative solutions, inclusive practices, and impactful actions to foster a sustainable and equitable future for all.`,
+        that generate profits while considering social, environmental, and economic factors. Fast paced global challenges such as climate change, resource depletion, and social inequality 
+        require innovative solutions that balance economic growth with environmental and social responsibility.`,
         links:[]
     },
     {
@@ -79,6 +120,11 @@ export const december = [
         emphasizes partnership and solidarity among individuals, communities, and states in all stages of epidemic management. It recognizes the primary role 
         of governments and the indispensable contributions of stakeholders, especially women, who constitute the majority of health workers. It also promotes 
         the exchange of information, scientific knowledge, and best practices on epidemics at local, national, regional, and global levels.`,
-        links:[]
+        links:[
+            {
+                label: "who.int",
+                value: "https://www.who.int"
+            }
+        ]
     }
 ]
